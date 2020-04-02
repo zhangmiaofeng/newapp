b40639c3ad4aab615874f29f3c627325a96001cd
zhangmiaofeng <843119483@qq.com>
2020-04-02 19:59:29 +0800

    配置login登录模块路由完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,19 @@ import VueRouter from 'vue-router'
 
 import Vue from 'vue'
 
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+// 配置路由规则
+  routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
+  ]
 })
 
 export default router
