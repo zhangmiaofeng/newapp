a59e10104cc1b5f54d464e771f566f14868a8b8f
zhangmiaofeng <843119483@qq.com>
2020-04-02 19:29:08 +0800

    安装element配置路由

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 引入路由
+import router from '@/router'
+
+// 挂载
+Vue.use(ElementUI)
 // I在控制台提示信息
 Vue.config.productionTip = false
 
@@ -10,5 +19,6 @@ Vue.config.productionTip = false
 
 // 创建一个根实例 渲染根组件 挂载在App组件
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import VueRouter from 'vue-router'
+
+import Vue from 'vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: []
+})
+
+export default router
