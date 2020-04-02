c74764610bfd594b516bd6a562d19a1657b949d0
zhangmiaofeng <843119483@qq.com>
2020-04-02 14:48:59 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// I在控制台提示信息
 Vue.config.productionTip = false
 
+// main.js
+// 职责：导入项目需要的依赖资源 js css 等等其他资源
+// 创建一个根实例
+
+// 创建一个根实例 渲染根组件 挂载在App组件
 new Vue({
   render: h => h(App)
 }).$mount('#app')
