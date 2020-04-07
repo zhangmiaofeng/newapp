7396f34a6821c94e069cde3807bd84df45b81df5
zhangmiaofeng <843119483@qq.com>
2020-04-07 18:43:52 +0800

    实现分页功能

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -24,9 +24,9 @@
           <el-select v-model="reqParamse.channel_id" multiple placeholder="请选择">
             <el-option
               v-for="item in channelOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
+              :key="item.id"
+              :label="item.name"
+              :value="item.id">
             </el-option>
           </el-select>
         </el-form-item>
@@ -48,16 +48,25 @@
     <el-card>
       <!-- 具名插槽 -->
       <div slot="header">
-        根据筛选条件查询到 0条 结果
+        根据筛选条件查询到 {{total}}条 结果
       </div>
       <!-- 表格组件 -->
       <el-table
         :data="articles"
         style="width: 100%">
         <el-table-column
-          prop="img"
+          prop="images"
           label="封面"
           width="180">
+          <!-- 使用作用域插槽 -->
+          <template slot-scope="scope">
+            <!-- {{scope.row}} -->
+            <el-image :src="scope.row.cover.images[0]" fit="cover" style="width:120px;height:80px">
+              <div slot="error" class="image-slot">
+                <img class="el-icon-picture-outline" style="width:120px;height:80px">
+              </div>
+            </el-image>
+          </template>
         </el-table-column>
         <el-table-column
           prop="title"
@@ -67,6 +76,22 @@
         <el-table-column
           prop="status"
           label="状态">
+          <template slot-scope="scope">
+            <el-tag v-if="scope.row.status === 0" type="info">草稿</el-tag>
+            <el-tag v-if="scope.row.status === 1">待审核</el-tag>
+            <el-tag v-if="scope.row.status === 2" type="successfo">审核通过</el-tag>
+            <el-tag v-if="scope.row.status === 3" type="warning">审核失败</el-tag>
+            <el-tag v-if="scope.row.status === 4" type="danger">删除</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column
+          prop="pubdate"
+          label="操作"
+          width="120">
+          <template>
+            <el-button plain type="primary" icon="el-icon-edit" circle></el-button>
+            <el-button plain type="danger" @click="del(scope.row.id)" icon="el-icon-delete" circle></el-button>
+          </template>
         </el-table-column>
         <el-table-column
           prop="pubdate"
@@ -78,7 +103,10 @@
         <el-pagination
           background
           layout="prev, pager, next"
-          :total="1000">
+          :total="total"
+          :page-size="reqParamse.per_page"
+          :current-page="reqParamse.page"
+          @current-change="changePager">
         </el-pagination>
       </div>
     </el-card>
@@ -94,26 +122,46 @@ export default {
       // 筛选项表单数据,提交给后台参数
       reqParamse: {
         status: null,
-        channel_id: null
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null,
+        page: 1,
+        per_page: 20
       },
       // 频道的下拉选项数据
-      channelOptions: [
-        { value: 1, label: '绩效考核' }
-      ],
+      channelOptions: [],
       // 时间日期
       dateArr: [],
       // 表格数据
-      articles: [
-        { img: '1', title: '', status: '', pubdate: '' }
-      ]
+      articles: [],
+      total: 0
     }
   },
   // 在创建实例之后发请求 获取数据
   created () {
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles')
-      .then((res) => {
-        console.log(res)
-      })
+    // 获取频道下拉选项数据
+    this.getChannelOptions()
+    this.getArticles()
+  },
+  methods: {
+    // 获取频道下拉列表数据
+    async getChannelOptions () {
+      const { data: { data } } = await this.$http.get('channels')
+      // console.log(data)
+      this.channelOptions = data.channels
+    },
+    // 获取筛选结果数据 携带参数
+    async getArticles () {
+      const { data: { data } } = await this.$http.get('articles', { paramse: this.reqParamse })
+      console.log(data)
+      this.articles = data.results
+      this.total = data.total_count
+    },
+    // 改变分页
+    changePager (newPage) {
+      this.reqParamse.page = newPage
+      this.getArticles()
+    }
   }
 }
 </script>
